f5cd0cbdaea7f121fa612a7f78c82c089a6f9a48
xxbeann <yjb4945@gmail.com>
2024-06-13 15:52:58 +0900

    solved7

--- Faculty/assignment/과제5/5-7.js
+++ Faculty/assignment/과제5/5-7.js
@@ -1,29 +1,32 @@
 let tds;
 let prevIndex=0, index=0;
+let colors = ["red", "lightblue", "lightgreen", "lightcoral", "lightgoldenrodyellow", "lightgray"];
+
 window.onload = function () { // 웹 페이지의 로딩 완료시 실행
     tds = document.getElementsByTagName("td");	
     tds[index].style.backgroundColor = "red";
 }
+
 window.onkeydown = function (e) {
     switch(e.key) {
         case "ArrowDown" : 
-            if(index/3 >= 2) return; // 맨 위 셀의 경우
-            index += 3;
+            if(index/6 >= 5) return; // 맨 아래 셀의 경우
+            index += 6;
             break;
         case "ArrowUp" : 
-            if(index/3 < 1) return; // 맨 아래 셀의 경우
-            index -= 3;
+            if(index/6 < 1) return; // 맨 위 셀의 경우
+            index -= 6;
             break;
         case "ArrowLeft" : 
-            if(index%3 == 0) return; // 맨 왼쪽 셀의 경우
+            if(index%6 == 0) return; // 맨 왼쪽 셀의 경우
             index--; 
             break;
         case "ArrowRight" : 
-            if(index%3 == 2) return; // 맨 오른쪽 셀의 경우
+            if(index%6 == 5) return; // 맨 오른쪽 셀의 경우
             index++; 
             break;
     }
-    tds[index].style.backgroundColor = "orchid";
+    tds[index].style.backgroundColor = colors[Math.floor(index / 6)];
     tds[prevIndex].style.backgroundColor = "white";
     prevIndex = index;
 }
